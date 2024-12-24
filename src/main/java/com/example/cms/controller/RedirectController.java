0d08ce7a78af6cb9aea9d0d6c3907b7cf7dfb155
Hayden <nguyenkhacthe94@gmail.com>
2024-12-25 00:20:05 +0700

    add redirect controller

--- src/main/java/com/example/cms/controller/RedirectController.java
+++ src/main/java/com/example/cms/controller/RedirectController.java
@@ -1,4 +1,16 @@
 package com.example.cms.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.view.RedirectView;
+
+@RestController
 public class RedirectController {
+
+    @GetMapping("/")
+    public RedirectView redirectToApiPosts() {
+        return new RedirectView("/api/posts");
+    }
 }
